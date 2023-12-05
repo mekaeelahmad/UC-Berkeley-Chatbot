65bf311a6acca703794dadbbbf6c8664e801c2fa
bonnie-w <bonnie.wang.913@gmail.com>
2023-12-05 14:14:57 -0800

    attempt to connect back and frontend

--- app.py
+++ app.py
@@ -10,14 +10,22 @@ def index():
 
 @app.route('/ask', methods=['POST'])
 def ask():
-    data = request.json
-    user_query = data['query']
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=f"This user has a question related to UC Berkeley's classes. Their query is: {user_query}. [Additional context and URLs]",
-        max_tokens=150
-    )
-    return jsonify(answer=response.choices[0].text.strip())
+    try:
+        data = request.json
+        user_query = data['query']
+        
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=f"This user has a question related to UC Berkeley's classes. Their query is: {user_query}. [Additional context and URLs]",
+            max_tokens=150
+        )
+        
+        answer = response.choices[0].text.strip()
+        
+        return jsonify(answer=answer)
+
+    except Exception as e:
+        return jsonify(error=str(e)), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
